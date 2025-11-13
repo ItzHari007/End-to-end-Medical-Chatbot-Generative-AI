c0f021375e76efc75e88961eacdb2d61c83d792a
Hariprasanth Murugan <hm757447@gmail.com>
2025-11-14 00:37:30 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Pragatheesh M',
-    author_email= 'pragatheesh1729@gmail.com',
+    author= 'Hariprasanth M',
+    author_email= 'hm757447@gmail.com',
     packages= find_packages(),
     install_requires = []
 
